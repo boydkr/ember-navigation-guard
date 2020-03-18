56010641e54fe4b9c2cf957d57953bcde4abe42b
Kyle Boyd <kboyd@skillsengine.com>
2020-03-17 21:02:15 -0500

    Install addon-docs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,30 @@
-import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import { inject as service } from '@ember/service';
 
-export default class Router extends EmberRouter {
+export default class Router extends AddonDocsRouter {
+  @service navigationGuard;
+
   location = config.locationType;
   rootURL = config.rootURL;
+
+  willTransition(_oldRoute, _newRoute, transition) {
+    super.willTransition(...arguments);
+    if (
+      this.navigationGuard.preventNav &&
+      !window.confirm(
+        this.navigationGuard.getMessage()
+      )
+    ) {
+      transition.abort();
+    } else {
+      // Bubble the `willTransition` action so that
+      // parent routes can decide whether or not to abort.
+      return true;
+    }
+  }
 }
 
 Router.map(function() {
+  docsRoute(this, function() {});
 });
