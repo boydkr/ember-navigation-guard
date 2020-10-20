15e1cd4fdfda670e22f31259fb1af5bd44f4bcdf
Kyle Boyd <github.veritas@xoxy.net>
2020-10-20 14:21:20 -0500

    go back to ember-lodash

--- addon/services/navigation-guard.js
+++ addon/services/navigation-guard.js
@@ -2,7 +2,7 @@ import Service from '@ember/service';
 import emberWindow from 'ember-window-mock';
 import { tracked } from '@glimmer/tracking';
 import { TrackedMap } from 'tracked-maps-and-sets';
-import { some, find, findLast } from 'lodash-es/collection';
+import { some, find, findLast } from 'lodash/collection';
 
 class GuardReg {
   @tracked guarding = false;
