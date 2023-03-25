a7818039f941277735252269c8ddf3c77daeaf03
Kyle Boyd <github.veritas@xoxy.net>
2023-03-25 06:07:35 +0000

    destroy tabs

--- config/ember-try.js
+++ config/ember-try.js
@@ -60,9 +60,9 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': await getChannelURL('release'),
           },
-	  overrides: {
-	    'ember-source': '$ember-source',
-	  },
+          overrides: {
+            'ember-source': '$ember-source',
+          },
         },
       },
       {
@@ -71,9 +71,9 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': await getChannelURL('beta'),
           },
-	  overrides: {
-	    'ember-source': '$ember-source',
-	  },
+          overrides: {
+            'ember-source': '$ember-source',
+          },
         },
       },
       embroiderSafe(),
