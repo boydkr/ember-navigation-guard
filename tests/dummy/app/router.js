0cafa0fb28a36eb89c31be5665adbf493256e2a2
Kyle Boyd <github.veritas@xoxy.net>
2023-03-25 04:41:35 +0000

    mess with docs, add ember-data?

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,28 +1,34 @@
 import config from 'dummy/config/environment';
 import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 
 export default class Router extends AddonDocsRouter {
   @service navigationGuard;
+  @service router;
 
   location = config.locationType;
   rootURL = config.rootURL;
+  constructor() {
+    super(...arguments);
 
-  willTransition(_oldRoute, _newRoute, transition) {
-    super.willTransition(...arguments);
-    if (
-      this.navigationGuard.preventNav &&
-      !window.confirm(this.navigationGuard.getMessage())
-    ) {
-      transition.abort();
-    } else {
-      // Bubble the `willTransition` action so that
-      // parent routes can decide whether or not to abort.
-      return true;
-    }
+    this.router.on('routeWillChange', async (transition) => {
+      if (
+        this.navigationGuard.preventNav &&
+        !window.confirm(this.navigationGuard.getMessage())
+      ) {
+        transition.abort();
+      } else {
+        // Bubble the `willTransition` action so that
+        // parent routes can decide whether or not to abort.
+        return true;
+      }
+    });
   }
 }
 
 Router.map(function () {
-  docsRoute(this, function () {});
+  docsRoute(this, function () {
+    this.route('index');
+  });
 });
